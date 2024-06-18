2ccba6130830e163e56ef82e92e936113b19ea36
adeliadev <arodriguesthamyres@gmail.com>
2024-06-18 17:36:35 -0300

    [feat] Adicionando estrutura de conhecimentos e projetos

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('scrollDown', 'scrollDownImg').addEventListener('click', function() {
+    window.scrollTo({
+        top: 650,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('scrollDownImg').addEventListener('click', function() {
+    window.scrollTo({
+        top: 650,
+        behavior: 'smooth'
+    });
+});
